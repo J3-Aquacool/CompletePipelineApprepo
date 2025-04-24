ee0d407988a728761c9dcff25f198726b39d45e3
J3-Aquacool <jagdish.java2@gmail.com>
2025-04-24 11:51:07 +0530

    first commit

---
+++ src/main/java/com/mycode/anlyse/CompletePipeLineAppApplication.java
@@ -0,0 +1,13 @@
+package com.mycode.anlyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletePipeLineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletePipeLineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycode/anlyse/CompletePipeLineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.anlyse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletePipeLineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
